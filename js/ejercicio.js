587b04a175456dcb153f1476f4f5823f98038568
Yhos <guayabala565@gmail.com>
2026-01-07 09:50:38 -0600

    Practica02
    add: se agg el archivo del ejecrcio 01- Delaracion de variables, aun sin terminar

---
+++ js/ejercicio.js
@@ -0,0 +1,13 @@
+//1. Declaracion de variables utilizando el prefijo VAR
+var miNombre ="Yhostin M.";
+var misApellidos;
+//utilizaremos la funcion console.log para imprimir el valor actual de la variable
+console.log("El valor de la variablemiNombre es =",miNombre);
+console.log("El valor de la variable misapellidos es =",misApellidos);
+//esto arroja un undefined dado que la variable
+misApellidos="Ramirez Gonzalez";
+//En este momento la variable
+console.log("El valor de la variable misApellidos es=",  misApellidos);
+//para actualizar
+miNombre="Yhostin M.";
+console.log("hola, ",miNombre,misApellidos)
